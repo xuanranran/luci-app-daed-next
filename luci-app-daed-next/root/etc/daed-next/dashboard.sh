bc3b0a283b5c7e0160b84138507664934568110f
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2024-01-16 12:01:36 +0800

    fix: exist mountpoints

--- luci-app-daed-next/root/etc/daed-next/dashboard.sh
+++ luci-app-daed-next/root/etc/daed-next/dashboard.sh
@@ -1,25 +1,37 @@
+#!/bin/sh
+
 NODE_BIN=/usr/bin/node
 WEB_SQUASHFS=/usr/share/daed-next/daed-web.squashfs
 PID_FILE=/tmp/log/daed-next/dashboard.pid
+DAED_NEXT_DIR=/var/daed-next
 
-# 检查按钮状态
-if [ -e "$PID_FILE" ]; then
-    # 如果 PID 文件存在，说明进程已经在运行，因此关闭它
-    PID=$(cat "$PID_FILE")
-    kill $PID
-    rm "$PID_FILE"
-    sync && echo 3 > /proc/sys/vm/drop_caches
-    umount /var/daed-next
-else
-    # 如果 PID 文件不存在，说明进程未在运行，因此启动它
+start_server() {
     listen_port=$(uci -q get daed-next.config.listen_port)
-    
-    [ ! -d /var/daed-next ] && mkdir -p /var/daed-next
-    mount -t squashfs $WEB_SQUASHFS /var/daed-next
-    
+    if [ ! -d "$DAED_NEXT_DIR" ]; then
+        mkdir -p "$DAED_NEXT_DIR"
+    fi
+    mount -t squashfs "$WEB_SQUASHFS" "$DAED_NEXT_DIR" || { echo "Mount failed"; }
+
     ARGS="PORT=$listen_port HOSTNAME=0.0.0.0"
-    
-    # 启动 Node.js 服务器，并将其 PID 写入文件
-    /bin/sh -c "$ARGS $NODE_BIN /var/daed-next/server.js" &
+    /bin/sh -c "$ARGS $NODE_BIN $DAED_NEXT_DIR/server.js" &
     echo $! > "$PID_FILE"
+}
+
+stop_server() {
+    if [ -f "$PID_FILE" ]; then
+        PID=$(cat "$PID_FILE")
+        kill "$PID" || { echo "Failed to kill process $PID"; }
+        rm -f "$PID_FILE"
+
+        mount_points=$(mount | grep '/tmp/daed-next' | awk '{print $3}')
+        for mp in $mount_points; do
+            umount -l "$mp" || { echo "Failed to force unmount $mp"; }
+        done
+    fi
+}
+
+if [ -e "$PID_FILE" ]; then
+    stop_server
+else
+    start_server
 fi
